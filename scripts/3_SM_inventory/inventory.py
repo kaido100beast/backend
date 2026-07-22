d47924bc5a1d3bd7b0eddae840874a3a3fa6148f
kaido100beast <amarkbpk@gmail.com>
2026-07-22 21:11:49 +0530

    day 6 - commit

---
+++ scripts/3_SM_inventory/inventory.py
@@ -0,0 +1,30 @@
+from smart_meter import SmartMeter
+from dataclasses import dataclass
+
+
+@dataclass
+class MeterInventory:
+    meters : dict[str, SmartMeter]
+
+    def add_meter(self, meter: SmartMeter) -> None:
+        self.meters[meter.meter_id] = meter
+
+    def remove_meter(self, meter_id: str) -> None:
+        try:
+            print(f"meter {self.meters.pop(meter_id)} deleted!")
+        except KeyError:
+            print("Key not found!")
+
+    def find_meter(self, meter_id: str) -> SmartMeter:
+            return self.meters.get(meter_id)
+
+    def display_all(self) -> None:
+        for meter in self.meters.values():
+            meter.display()
+
+    def connected_meters(self) -> list[SmartMeter]:
+        connected_meters = []
+        for meter in self.meters.values():
+            if meter.status == "Connected":
+                connected_meters.append(meter)
+        return connected_meters
--- scripts/3_SM_inventory/models.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class SmartMeter:
-    meter_id: str
-    serial_number: str
-    firmware_version: str
-    status: str
